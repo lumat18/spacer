e223c8f46b85b491b17b92993d5a7cb3df745b04
lumat <lumat@op.pl>
2020-06-25 23:41:12 +0200

    setup formatting

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,21 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import About from '../views/About.vue';
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component() {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue');
+export default new VueRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
     },
-  },
-];
-
-const router = new VueRouter({
-  routes,
+    {
+      path: '/about',
+      name: 'about',
+      component: About,
+    },
+  ],
 });
-
-export default router;
