cbde7431050b3229f87788845bae35c62aa761fa
lumat <lumat@op.pl>
2020-06-26 01:49:42 +0200

    remove eslint

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,16 @@
   <div class="app">
     <div class="nav">
       <router-link to="/">spacer</router-link>
-      <router-link to="/about">about</router-link>
     </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss" scoped>
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Montserrat', sans-serif;
+}
 </style>
