e062d6cca870b0c13830b6fc1a25cca979f57e2b
lumat <lumat@op.pl>
2020-06-28 17:14:33 +0200

    style input form

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -18,13 +18,22 @@ export default {
   margin-top: 50px;
   display: flex;
   flex-direction: column;
-  width: 300px;
+  width: 250px;
 
   input {
     height: 30px;
+    color: white;
+    text-align: center;
+    font-size: 18px;
+    font-weight: 300;
     border: 0;
     border-bottom: 1px solid white;
     background: none;
+  }
+
+  input:focus {
+    outline: none;
+    box-shadow: 0 5px 10px -8px white;
   }
 }
 </style>
